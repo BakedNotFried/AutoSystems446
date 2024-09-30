669a6648f7965e9df5ff2574471879c6ce0f7bb0
Cameron Mckiddie <cameronsmowing@gmail.com>
2024-09-30 15:08:03 +1000

    path planning + path following WIP

--- setupParams.m
+++ setupParams.m
@@ -1,21 +1,8 @@
 %% Setup inital values for the simulation
-function [scanAngles, path_a_star] = setupParams()
+function [scanAngles] = setupParams()
     % LiDAR
     fov = 60;
     fov = fov * pi/180;
-    numReturns = 25;
+    numReturns = 3;
     scanAngles = linspace(-fov, fov, numReturns);
-
-    % Basic A* Path
-    cplx_map = load('complexMap_air_ground.mat');
-    map = cplx_map.map;
-    
-    % init a planner....
-    planner = plannerAStarGrid(map);
-
-    % Define a a start and goal...
-    start = [20 20];
-    goal = [248 248];
-
-    path_a_star = plan(planner,start,goal);
 end
